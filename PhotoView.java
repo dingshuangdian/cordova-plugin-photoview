a9c70e72d06c024cc383503c8e4264b231920627
dingshuangdian <286502698@qq.com>
2018-08-10 09:28:21 +0800

    photoview

---
+++ PhotoView.java
@@ -0,0 +1,48 @@
+package photoview;
+
+import android.app.Activity;
+import android.content.Intent;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+
+import photoview.photoview.ImageGalleryActivity;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class PhotoView extends CordovaPlugin {
+  private List<String> list = new ArrayList<>();
+  private Activity cordovaActivity;
+
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+    cordovaActivity = cordova.getActivity();
+  }
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("getUrl")) {
+      list.clear();
+      JSONObject jsonObject = args.getJSONObject(0);
+      String url = jsonObject.optString("url");
+      JSONArray jsonArray = new JSONArray(url);
+      for (int i = 0; i < jsonArray.length(); i++) {
+        JSONObject object = (JSONObject) jsonArray.get(i);
+        String tarUrl = object.getString("url").replace("\\", "/");
+        list.add(tarUrl);
+      }
+      Intent intent = new Intent(cordovaActivity, ImageGalleryActivity.class);
+      intent.putStringArrayListExtra("images", (ArrayList<String>) list);
+      cordovaActivity.startActivity(intent);
+    }
+    return true;
+  }
+}
---
+++ photoview/ImageGalleryActivity.java
@@ -0,0 +1,66 @@
+package photoview.photoview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import java.util.ArrayList;
+import java.util.List;
+public class ImageGalleryActivity extends FragmentActivity {
+  private List<String> imgUrls; //图片列表
+  private Button headBackBtn;
+  private ViewPager mViewPager;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+//取消状态栏
+    /*getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+      WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
+    setContentView(this.getResources().getIdentifier("activity_touch_gallery",
+      "layout", this.getPackageName()));
+    Intent intent = getIntent();
+    imgUrls = intent.getStringArrayListExtra("images");
+    if (imgUrls == null) {
+      imgUrls = new ArrayList<>();
+    }
+    initView();
+    initViewEvent();
+    initGalleryViewPager();
+  }
+
+  private void initView() {
+    headBackBtn = findViewById(this.getResources().getIdentifier("btnBack", "id", this.getPackageName()));
+    headBackBtn.setVisibility(View.VISIBLE);
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+  }
+
+  private void initViewEvent() {
+    headBackBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        finish();
+      }
+    });
+  }
+  private void initGalleryViewPager() {
+    PhotoViewAdapter pagerAdapter = new PhotoViewAdapter(this, imgUrls);
+    pagerAdapter.setOnItemChangeListener(new PhotoViewAdapter.OnItemChangeListener() {
+      @Override
+      public void onItemChange(int currentPosition) {
+
+      }
+    });
+    mViewPager = findViewById(this.getResources().getIdentifier("viewer", "id", this.getPackageName()));
+    mViewPager.setOffscreenPageLimit(3);
+    mViewPager.setAdapter(pagerAdapter);
+  }
+
+}
